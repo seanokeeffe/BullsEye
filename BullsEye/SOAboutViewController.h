0f2c1e9a904e4339700a6b241b9a8bb65c6f62c2
seanokeeffe <seanokeeffe28@gmail.com>
2014-09-05 12:53:49 -0700

    Added Info Button Functionalities. New View Controller that manages the about bulls eye page.

---
+++ BullsEye/SOAboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  SOAboutViewController.h
+//  BullsEye
+//
+//  Created by Sean on 9/5/14.
+//  Copyright (c) 2014 Sean O'Keeffe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SOAboutViewController : UIViewController
+
+- (IBAction)close;
+
+@end
---
+++ BullsEye/SOAboutViewController.m
@@ -0,0 +1,60 @@
+//
+//  SOAboutViewController.m
+//  BullsEye
+//
+//  Created by Sean on 9/5/14.
+//  Copyright (c) 2014 Sean O'Keeffe. All rights reserved.
+//
+
+#import "SOAboutViewController.h"
+
+@interface SOAboutViewController ()
+
+@end
+
+@implementation SOAboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/**
+ * Method that gets called when the 'Close' button is clicked
+ * Dismisses this view controller
+ */
+- (IBAction)close
+{
+    
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
